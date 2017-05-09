691b5f85a7485b14f1cef565c311c34c0bec0d56
Felix Gräßer <felix.graesser@tu-dresden.de>
2017-05-09 11:11:14 +0200

    added

---
+++ therapien_rs.php
@@ -0,0 +1,72 @@
+<?php
+
+function show_therapien_rs() {
+
+// Daten Patient
+    $patient = $_SESSION['idPatient'];
+
+// Therapien
+    $results = mysql_query("SELECT * FROM tbltherapierecom WHERE ingTyp = 2 ORDER BY Indicator DESC");
+    $therapies = array();
+    while ($row = mysql_fetch_array($results)) {
+        $indicator[$row['IDTherapie']] = $row['Indicator'];
+        $therapies[$row['IDTherapie']] = $row['txtName'];
+        $info[$row['IDTherapie']] = $row['Information'];
+        $infoTyp[$row['IDTherapie']] = $row['InfoTyp'];
+    }
+//    print_r($info);
+    ?>
+
+    <form class="questionblock" action="" method="post">
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Indikator</th>
+                    <th>Therapie</th>
+                    <th>Information</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $therapieCnt = 0;
+                $numRecom = 3;
+                $n = count($therapies);
+                $factor = ($n^2+$n) / 2 + 1;
+                foreach ($therapies as $id => $val) {
+                    
+                    echo "<tr style=\"background-color:#b3e6ff;\">";
+
+                    if ($infoTyp[$id] == 1) {
+                        echo "<tr style=\"background-color:#66f;\">";
+                        $therapieCnt ++;
+                    }
+
+                    if ($infoTyp[$id] == 2) {
+                        echo "<tr style=\"background-color:#ff9;\">";
+                    }
+
+                    if ($infoTyp[$id] == 3) {
+                        echo "<tr style=\"background-color:#ffd9b3;\">";
+                    }
+
+                    if ($infoTyp[$id] == 1 && $therapieCnt > $numRecom) {
+                        echo "<tr style=\"background-color:#b3e6ff;\">";
+                    }
+                    ?>
+                <td><b><?php echo round($indicator[$id] / $factor,2) ?></b></td>
+                <td><b><?php echo $val ?></b></td>
+                <td><b><?php echo $info[$id] ?></b></td>             
+
+                </tr>
+                <?php
+            }
+            ?>
+            </tbody>
+        </table>
+
+    </form>
+
+    <?php
+}
+?>
